2552615916901d5f5eaa02ee00e50c114cc514a4
george <mr@georgee.co.uk>
2019-12-30 14:48:05 +0400

    fixed timing

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -4,8 +4,8 @@ import style from './contact.module.scss'
 
 const encode = (data) => {
   return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 }
 
 export default class ContactForm extends Component {
@@ -16,83 +16,85 @@ export default class ContactForm extends Component {
     error: false,
     slide: false,
   };
-  
+
   handleChange = (e) => {
-    this.setState({[e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   // change submitting button text on error
   handleButton = () => {
-    this.setState({error: true});
+    this.setState({ error: true });
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
     // on successful submitting, change button text
-    this.setState({error: false, slide: true });
+    this.setState({ error: false, slide: true });
 
     fetch("/", {
       method: "POST",
-      headers: {"Content-Type": "application/x-www-form-urlencoded"},
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...this.state })
     })
-    .then(() => console.log("Message was successfully send!"))
-    .catch(console.error)
+      .then(() => console.log("Message was successfully send!"))
+      .catch(console.error)
   }
 
   render() {
     // on successful submitting, slide button animation
-    const slideButton =  this.state.slide ? `${style.button_slide}` : `${style.button}`
+    const slideButton = this.state.slide ? `${style.button_slide}` : `${style.button}`
     // on successful submitting, thanks message animation
-    const slideText =  this.state.slide ?  `${style.thanks_active}` : `${style.thanks}`
+    const slideText = this.state.slide ? `${style.thanks_active}` : `${style.thanks}`
 
     // Determines when contact-form was send. : ))
     const date = new Date()
     const hours = date.getHours()
     let timeOfDay
-    let timeOfDayMid
-    
-    if (hours < 12) {
-      timeOfDay = ", have a nice day."
+    let timeOff
+
+    if (hours >= 5 && hours < 12) {
+      timeOfDay = "Good morning,"
     } else if (hours >= 12 && hours < 17) {
-      timeOfDayMid = "Good afternoon,"
-    } else {
-      timeOfDay = ", have a nice time." 
+      timeOfDay = "Good afternoon,"
+    } else if (hours >= 18 && hours < 21) {
+      timeOfDay = "Good Evening,"
+    } else if (hours >= 21 || hours < 5) {
+      timeOff = ", good night."
     }
 
     return (
-        <form className={style.form}
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={this.handleSubmit}
-        >
-          <h1 className={style.title}>Get in touch</h1>
+      <form className={style.form}
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={this.handleSubmit}
+      >
+        <h1 className={style.title}>Get in touch</h1>
 
-          <div className={style.field}>
-            <input type="text" name="name" onChange={this.handleChange} required />
-            <label htmlFor="name" placeholder="NAME"></label>
-          </div>
+        <div className={style.field}>
+          <input type="text" name="name" onChange={this.handleChange} required />
+          <label htmlFor="name" placeholder="NAME"></label>
+        </div>
 
-          <div className={style.field}>
-            <input type="email" name="email" onChange={this.handleChange} required />
-            <label htmlFor="email" placeholder="EMAIL"></label>
-          </div>
+        <div className={style.field}>
+          <input type="email" name="email" onChange={this.handleChange} required />
+          <label htmlFor="email" placeholder="EMAIL"></label>
+        </div>
 
-          <div className={style.field}>
-            <textarea name="message" onChange={this.handleChange} required /> 
-            <label htmlFor="message" placeholder="MESSAGE"></label>
-          </div>
+        <div className={style.field}>
+          <textarea name="message" onChange={this.handleChange} required />
+          <label htmlFor="message" placeholder="MESSAGE"></label>
+        </div>
 
-          {/* Spam Filtering using 'netlify-honeypot' instead of reCAPTCHA2 */}
-          <label className={style.hidden}>Don’t fill this out if you're human: <input name="bot-field" onChange={this.handleChange}/></label>
+        {/* Spam Filtering using 'netlify-honeypot' instead of reCAPTCHA2 */}
+        <label className={style.hidden}>Don’t fill this out if you're human: <input name="bot-field" onChange={this.handleChange} /></label>
 
-          <button className={slideButton} type="submit" onClick={this.handleButton} style={this.state.error ? {border:'solid 1px red'} : null}>
-            {this.state.error ? 'error occurred while Sending a Message' : 'Send Message'}
-          </button>
-          
-          <span className={slideText}>{timeOfDayMid} Thanks {this.state.name} for your message {timeOfDay}</span>
-            
-        </form>
+        <button className={slideButton} type="submit" onClick={this.handleButton} style={this.state.error ? { border: 'solid 1px red' } : null}>
+          {this.state.error ? 'error occurred while Sending a Message' : 'Send Message'}
+        </button>
+
+        <span className={slideText}>{timeOfDayMid} Thanks {this.state.name} for your message {timeOfDay}</span>
+
+      </form>
     )
   }
 }
